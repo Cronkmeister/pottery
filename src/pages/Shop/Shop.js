608bd569c8ffa690629586d15d0e002a0a83247c
Jonny Cronk <cronkmeister@Jonnys-MacBook-Pro.local>
2022-08-24 18:19:55 -0700

    shop page init

--- src/pages/Shop/Shop.js
+++ src/pages/Shop/Shop.js
@@ -1,5 +1,15 @@
 import React from "react";
+import "./Shop.scss";
 
 export default function Shop() {
-  return <div>Shop</div>;
+  return (
+    <div className="shop">
+      <div className="shop__banner">
+        <h1 className="shop__banner--text">Shop Current Creations</h1>
+        <div className="shop__banner--overlay"></div>
+      </div>
+      <div className="shop__wrapper"></div>
+      <div></div>
+    </div>
+  );
 }
