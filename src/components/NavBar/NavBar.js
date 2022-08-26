f2999fa12e7a1bd48630189ac1ce50bd9c7a03a3
Jonny Cronk <cronkmeister@Jonnys-MacBook-Pro.local>
2022-08-26 12:52:24 -0700

    shop style update

--- src/components/NavBar/NavBar.js
+++ src/components/NavBar/NavBar.js
@@ -7,25 +7,26 @@ import { Link } from "react-router-dom";
 
 export default function NavBar() {
   return (
-    <nav>
-      <Link className="logo" to="/">
-        <div className="logo__text">Rocky Studios</div>
-        <p className="logo__tagline">Artisan Pottery</p>
-      </Link>
-      <ul className="nav-links">
-        <Link className="nav-links__link" to="/shop">
-          Shop
+    <nav className="primary-navigation">
+      <div className="nav-wrapper">
+        <Link className="logo" to="/">
+          <div className="logo__text">Rocky Studios</div>
+          <p className="logo__tagline">Artisan Pottery</p>
         </Link>
-        <Link className="nav-links__link" to="/about">
-          About
-        </Link>
-        <Link className="nav-links__link" to="/contact">
-          Contact
-        </Link>
-        <VscAccount className="nav-links__icon" />
-        <AiOutlineShopping className="nav-links__icon" />
-      </ul>
-      {/* <div></div> */}
+        <ul className="nav-links">
+          <Link className="nav-links__link" to="/shop">
+            Shop
+          </Link>
+          <Link className="nav-links__link" to="/about">
+            About
+          </Link>
+          <Link className="nav-links__link" to="/contact">
+            Contact
+          </Link>
+          <VscAccount className="nav-links__icon" />
+          <AiOutlineShopping className="nav-links__icon" />
+        </ul>
+      </div>
     </nav>
   );
 }
