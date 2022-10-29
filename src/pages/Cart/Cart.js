1cdaeb84e3aa0b959b7b7cb93ff890f035fc4c99
Jonny Cronk <cronkmeister@Jonnys-MacBook-Pro.local>
2022-10-28 17:42:03 -0700

    cart structure pt1

--- src/pages/Cart/Cart.js
+++ src/pages/Cart/Cart.js
@@ -6,10 +6,24 @@ export default function Cart() {
     <>
       <NavBar />
       <div>Cart</div>
+      {/* left side container */}
       <div className="ls-container">
         <h1>my bag</h1>
-        <h3>&lt; items</h3>
+        <h3>(2 items)</h3>
+        {/* product card - repeatable  */}
+        <div className="product-card">
+          <img alt="product item"></img>
+          <h3>product title</h3>
+          <p>colour</p>
+          <p>category</p>
+          <p>colour</p>
+          <p>product id:</p>
+          <div className="product-details">
+            <p></p>
+          </div>
+        </div>
       </div>
+      {/* right side container */}
       <div className="rs-container">
         <h2>order summary</h2>
       </div>
